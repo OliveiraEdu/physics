7ec8a0f123a269d3c8a246af949b5981f59e7997
OliveiraEdu <40703876+OliveiraEdu@users.noreply.github.com>
2020-09-30 10:47:50 -0300

    Add files via upload

---
+++ FuncaoHoraria.py
@@ -0,0 +1,14 @@
+def S(t):
+    return 2+5*t
+
+print('A posição para este instante é:', S(3),'m')
+print('A posição para este instante é:', S(12),'m')
+print('A posição para este instante é:', S(377.4),'m')
+
+
+
+
+
+
+
+
